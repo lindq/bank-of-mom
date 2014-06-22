b0bc8cd84328237cc7631f7f2d223e389c215169
David Lindquist <david.lindquist@gmail.com>
2014-06-21 22:22:15 -0700

    Put all modules into app.js

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -1,22 +1,94 @@
 'use strict';
 
+var app = angular.module('bom', ['ngRoute']);
 
-angular.module('bom', [
-  'ngRoute',
-  'bom.services',
-  'bom.controllers'
-])
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-      .when('/accounts', {
-        templateUrl: 'partials/accounts/list.html',
-        controller: 'AccountListController'
-      })
-      .when('/accounts/:id', {
-        templateUrl: 'partials/accounts/detail.html',
-        controller: 'AccountDetailController'
-      })
-      .otherwise({
-        redirectTo: '/accounts'
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/accounts', {
+      templateUrl: 'partials/accounts/list.html',
+      controller: 'AccountListController'
+    })
+    .when('/accounts/:id', {
+      templateUrl: 'partials/accounts/detail.html',
+      controller: 'AccountDetailController'
+    })
+    .otherwise({
+      redirectTo: '/accounts'
+    });
+});
+
+app.factory('utils', function($q, $rootScope) {
+
+  var apiMethod = function(method) {
+    return function(message) {
+      $rootScope.loading = true;
+
+      var deferred = $q.defer();
+
+      method(message).execute(function(response) {
+        if (response.error) {
+          deferred.reject(response.message);
+        } else {
+          deferred.resolve(response);
+        }
+        $rootScope.loading = false;
       });
-  }]);
+
+      return deferred.promise;
+    }
+  };
+
+  return {
+    apiMethod: apiMethod
+  };
+})
+
+app.factory('AccountService', function(utils) {
+  var bom = gapi.client.bom;
+  return {
+    insert: utils.apiMethod(bom.accounts.insert),
+    list: utils.apiMethod(bom.accounts.list),
+    get: utils.apiMethod(bom.accounts.get),
+    remove: utils.apiMethod(bom.accounts.remove)
+  }
+})
+
+app.factory('TransactionService', function(utils) {
+  var bom = gapi.client.bom;
+  return {
+    insert: utils.apiMethod(bom.transactions.insert),
+    list: utils.apiMethod(bom.transactions.list),
+    get: utils.apiMethod(bom.transactions.get),
+    remove: utils.apiMethod(bom.transactions.remove)
+  }
+});
+
+app.controller('AccountListController', function($scope, AccountService) {
+  AccountService.list().then(function(response) {
+    $scope.accounts = response.items;
+  });
+})
+
+app.controller('AccountDetailController', function($scope, $routeParams, AccountService, TransactionService) {
+
+  AccountService.get({id: $routeParams.id})
+    .then(function(response) {
+      $scope.account = response;
+    });
+
+  var loadTransactions = function() {
+    TransactionService.list({
+      accountId: $routeParams.id,
+      nextPageToken: $scope.nextPageToken
+    })
+      .then(function(response) {
+        $scope.transactions = $scope.transactions.concat(response.items);
+        $scope.nextPageToken = response.nextPageToken;
+      });
+  };
+
+  $scope.transactions = [];
+  $scope.loadTransactions = loadTransactions;
+
+  loadTransactions();
+});
