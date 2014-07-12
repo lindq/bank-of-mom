03cce9a76daa8862745bdd34bd6ed649af0729ab
David Lindquist <david.lindquist@gmail.com>
2014-07-12 14:54:51 -0700

    Removed space

--- client/app/js/filters.js
+++ client/app/js/filters.js
@@ -2,7 +2,7 @@
 
 angular.module('bomFilters', [])
   .filter('dateLocalize', function() {
-    return function (utcDate) {
+    return function(utcDate) {
       return new Date(utcDate + 'Z').getTime();
     }
   });
