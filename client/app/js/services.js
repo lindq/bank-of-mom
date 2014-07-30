f496b9d81f42a8c089acada434e4ed8ee8de07c4
David Lindquist <david.lindquist@gmail.com>
2014-07-30 08:16:43 -0700

    Services refactor

--- client/app/js/services.js
+++ client/app/js/services.js
@@ -38,24 +38,23 @@ angular.module('bomServices', [])
     }
   })
   .factory('Auth', function($q, settings) {
-    var check = function(immediate) {
-      var deferred = $q.defer();
-      var params = {
-        client_id: settings.OAUTH_CLIENT_ID,
-        scope: settings.OAUTH_SCOPE,
-        immediate: immediate
-      }
-      gapi.auth.authorize(params, function(response) {
-        if (response.error) {
-          deferred.reject(response);
-        } else {
-          deferred.resolve(response);
-        }
-      });
-      return deferred.promise;
-    };
     return {
-      check: check
+      check: function(immediate) {
+        var deferred = $q.defer();
+        var params = {
+          client_id: settings.OAUTH_CLIENT_ID,
+          scope: settings.OAUTH_SCOPE,
+          immediate: immediate
+        }
+        gapi.auth.authorize(params, function(response) {
+          if (response.error) {
+            deferred.reject(response);
+          } else {
+            deferred.resolve(response);
+          }
+        });
+        return deferred.promise;
+      };
     }
   })
   .factory('Api', function($q, settings, Auth, Redirect) {
@@ -66,51 +65,50 @@ angular.module('bomServices', [])
                        callback, settings.API_PATH);
       return deferred.promise;
     };
-    var proxy = function(collection, method) {
-      return function(message) {
-
-        var call = function() {
-          var func = gapi.client[settings.API_NAME][collection][method];
-          var deferred = $q.defer();
-          func(message).execute(function(response) {
-            if (response.error) {
-              deferred.reject(response);
-            } else {
-              deferred.resolve(response);
-            }
-          });
-          return deferred.promise;
-        }
-
-        return Auth.check(true)
-          .then(angular.noop, Redirect.to('auth'))
-          .then(load)
-          .then(call);
-      }
-    };
     return {
-      proxy: proxy
+      method: function(collection, method) {
+        return function(message) {
+
+          var call = function() {
+            var func = gapi.client[settings.API_NAME][collection][method];
+            var deferred = $q.defer();
+            func(message).execute(function(response) {
+              if (response.error) {
+                deferred.reject(response);
+              } else {
+                deferred.resolve(response);
+              }
+            });
+            return deferred.promise;
+          }
+
+          return Auth.check(true)
+            .then(angular.noop, Redirect.to('auth'))
+            .then(load)
+            .then(call);
+        }
+      }
     }
   })
   .factory('Account', function(Api) {
     var collection = 'accounts';
     return {
-      get: Api.proxy(collection, 'get'),
-      insert: Api.proxy(collection, 'insert'),
-      list: Api.proxy(collection, 'list'),
-      patch: Api.proxy(collection, 'patch'),
-      remove: Api.proxy(collection, 'remove'),
-      update: Api.proxy(collection, 'update')
+      get: Api.method(collection, 'get'),
+      insert: Api.method(collection, 'insert'),
+      list: Api.method(collection, 'list'),
+      patch: Api.method(collection, 'patch'),
+      remove: Api.method(collection, 'remove'),
+      update: Api.method(collection, 'update')
     }
   })
   .factory('Transaction', function(Api) {
     var collection = 'transactions';
     return {
-      get: Api.proxy(collection, 'get'),
-      insert: Api.proxy(collection, 'insert'),
-      list: Api.proxy(collection, 'list'),
-      patch: Api.proxy(collection, 'patch'),
-      remove: Api.proxy(collection, 'remove'),
-      update: Api.proxy(collection, 'update')
+      get: Api.method(collection, 'get'),
+      insert: Api.method(collection, 'insert'),
+      list: Api.method(collection, 'list'),
+      patch: Api.method(collection, 'patch'),
+      remove: Api.method(collection, 'remove'),
+      update: Api.method(collection, 'update')
     }
   });
