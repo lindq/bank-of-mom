71a9d23c080fcb39feba8fc371d30458f16b19d7
David Lindquist <david.lindquist@gmail.com>
2014-08-10 22:34:48 -0700

    Updated compile script

--- compilejs.sh
+++ compilejs.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-COMPILER_URL=http://closure-compiler.appspot.com/compile
-
-JS_CODE_DIR="$PWD/client/app/js"
-JS_EXTERNS_DIR="$PWD/client/app/externs"
-
-for filename in $(find $JS_CODE_DIR -name \*.js); do
+for filename in $(find "$PWD/client/app/js" -name \*.js); do
     JS_CODE_OPTS="$JS_CODE_OPTS --data-urlencode js_code@$filename"
 done
 
@@ -16,4 +11,4 @@ curl \
     --data-urlencode "output_format=text" \
     --data-urlencode "output_info=compiled_code" \
     --data-urlencode "use_closure_library=true" \
-    $COMPILER_URL
+    http://closure-compiler.appspot.com/compile
