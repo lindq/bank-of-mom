1415b448d27220862b0fb8d8a515b27d864b7101
David Lindquist <david.lindquist@gmail.com>
2014-08-12 19:22:16 -0700

    Flag for output_info compiler option.

--- compilejs.sh
+++ compilejs.sh
@@ -8,15 +8,22 @@ for filename in $(find "$PWD/client/app/externs" -name \*.js); do
     JS_EXTERNS_OPTS="$JS_EXTERNS_OPTS --data-urlencode js_externs@$filename"
 done
 
+case "$1" in
+    warnings)
+        OUTPUT_INFO="--data-urlencode output_info=warnings"
+        OUTPUT_INFO="$OUTPUT_INFO --data-urlencode warning_level=VERBOSE" ;;
+    errors)
+        OUTPUT_INFO="--data-urlencode output_info=errors" ;;
+    *)
+        OUTPUT_INFO="--data-urlencode output_info=compiled_code" ;;
+esac
+
 curl \
     $JS_CODE_OPTS \
     $JS_EXTERNS_OPTS \
-    --data-urlencode "angular_pass=true" \
-    --data-urlencode "compilation_level=SIMPLE_OPTIMIZATIONS" \
-    --data-urlencode "output_format=text" \
-    --data-urlencode "output_info=compiled_code" \
-    --data-urlencode "output_info=warnings" \
-    --data-urlencode "output_info=errors" \
-    --data-urlencode "warning_level=VERBOSE" \
-    --data-urlencode "use_closure_library=true" \
+    $OUTPUT_INFO \
+    --data-urlencode angular_pass=true \
+    --data-urlencode compilation_level=SIMPLE_OPTIMIZATIONS \
+    --data-urlencode output_format=text \
+    --data-urlencode use_closure_library=true \
     http://closure-compiler.appspot.com/compile
